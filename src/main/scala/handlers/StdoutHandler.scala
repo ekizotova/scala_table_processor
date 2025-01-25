364a1d04c94feeb59e9c57a29d1a2abce7ff843b
Ekaterina <ekaterina.izotova.work@gmail.com>
2025-01-25 23:50:42 +0100

    Create StdoutHandler.scala

---
+++ src/main/scala/handlers/StdoutHandler.scala
@@ -0,0 +1,29 @@
+package handlers
+
+import filters.{ColumnConditionFilter, FilterIsEmpty, FilterIsNotEmpty, TableFilter}
+import formatters.MarkdownFormatter
+import loaders.FileLoader
+import parsers.CsvTableParser
+import evaluators.EnhancedFormulaEvaluator
+import csvParts.*
+
+import java.io.{File, PrintWriter}
+
+class StdoutHandler(var next: Option[CliHandler]) extends CliHandler {
+  def setNext(nextHandler: Option[CliHandler]): Unit = {
+    next = nextHandler
+  }
+  
+  override def handle(args: Array[String], options: scala.collection.mutable.Map[String, Any], filters: scala.collection.mutable.Buffer[TableFilter]): Array[String] = {
+    if (args.nonEmpty && args.head == "--stdout") {
+      options("stdout") = true
+      args.tail
+    } else {
+      next.map(_.handle(args, options, filters)).getOrElse(args)
+    }
+  }
+
+  override def describe(): Option[String] = Some(
+    "Output: --stdout : Print the table to the standard output (optional, by default true)."
+  )
+}
